c2f51fb7c7cad27b20d3ce5df20de1a5c5a2c771
Oskar Kling <oskarkling@gmail.com>
2021-11-12 11:18:52 +0100

    first commit

--- EnviormentTestCICD/Program.cs
+++ EnviormentTestCICD/Program.cs
@@ -6,7 +6,7 @@ namespace EnviormentTestCICD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worldsdsdsdd!");
         }
     }
 }
